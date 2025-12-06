91ea84e469e6307e4800d3bf2d728289c3e8381c
krishnamayekar <krishna.mayekar@aithinkers.com>
2025-12-06 13:12:19 +0530

    trest

--- github_webhook/app.py
+++ github_webhook/app.py
@@ -31,11 +31,7 @@ def lambda_handler(event, context):
     headers = event.get("headers", {})
     body = event.get("body", "")
 
-    print("-----DEBUG------")
-    print("Headers:", headers)
     print("Body:===>", body[:300])
-    print("Signature:", headers.get("X-Hub-Signature-256"))
-    print("----------------")
 
     if event.get("isBase64Encoded", False):
         body = base64.b64decode(body).decode("utf-8")
