e0ab2570ffeb4d556281eee49932cb0e68f1bc31
krishnamayekar <krishna.mayekar@aithinkers.com>
2025-12-15 15:27:30 +0530

    fixed apis

--- pr_bottleneck/app.py
+++ pr_bottleneck/app.py
@@ -8,7 +8,7 @@ DB_HOST = os.environ.get("DB_HOST")
 DB_USER = os.environ.get("DB_USER")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
 DB_NAME = os.environ.get("DB_NAME")
-PR_IDLE_THRESHOLD_DAYS = int(os.environ.get("PR_IDLE_THRESHOLD_DAYS", 3))  # define bottleneck threshold
+PR_IDLE_THRESHOLD_DAYS = int(os.environ.get("PR_IDLE_THRESHOLD_DAYS", 2))  # define bottleneck threshold
 
 def get_connection():
     return psycopg2.connect(
@@ -48,11 +48,29 @@ def lambda_handler(event, context):
             bottlenecks.append({
                 "pr_number": pr_number,
                 "title": title,
-                "author": author,
                 "state": state,
+                "author": author,
+
+                # Time
                 "idle_days": idle_days,
+                "last_updated": updated_at.isoformat(),
+                "last_updated_ago": f"{idle_days} days ago" if idle_days > 0 else "few hours ago",
+
+                # GitHub stats
+                "commits": raw_json.get("commits"),
+                "files_changed": raw_json.get("changed_files"),
+                "additions": raw_json.get("additions"),
+                "deletions": raw_json.get("deletions"),
+                "total_changes": (raw_json.get("additions", 0) + raw_json.get("deletions", 0)),
+
+                # Branch info
+                "base_branch": raw_json.get("base", {}).get("ref"),
+                "head_branch": raw_json.get("head", {}).get("ref"),
+
+                # URLs
                 "url": raw_json.get("html_url")
             })
+
     
     return {
         "statusCode": 200,
